a298d0ea7b930dda9e0014757a09316674dd9f0a
Pierre Snell <pierre.snell@ubisoft.com>
2025-08-01 12:53:15 -0400

    feat: rebuilt

--- docs/_app/immutable/chunks/C7NNqsKE.js
+++
@@ -1,1 +0,0 @@
-import{o as a}from"./Dpbo9POk.js";a();
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/jazz_midi' : '',
+		},
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
